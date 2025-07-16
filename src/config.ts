9ca8f430d1968b70a38a10e3baf4ccc956bf9cc9
TheWhiteWolves <jonsmith0@gmail.com>
2025-07-16 22:30:50 +0100

    finish chapter 3

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export type APIConfig = {
+    fileserverHits: number;
+};
+
+export const config: APIConfig = {fileserverHits: 0};
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import express from "express";
 
 import { handlerReadiness } from "./api/readiness.js";
-import { middlewareLogResponse } from "./api/middleware.js";
+import { middlewareLogResponse, middlewareMetricsInc } from "./api/middleware.js";
+import { handlerMetrics, handlerResetMetrics } from "./api/metrics.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponse);
+app.use("/app", middlewareMetricsInc);
 app.use("/app", express.static("./src/app"));
 
-app.get("/healthz", handlerReadiness);
+app.get("/api/healthz", handlerReadiness);
+app.get("/admin/metrics", handlerMetrics);
+app.get("/admin/reset", handlerResetMetrics);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
