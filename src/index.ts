fa383bcdabc3eafb83a7d7c397ec398e0b2966d1
TheWhiteWolves <jonsmith0@gmail.com>
2025-07-19 12:29:39 +0100

    Chapter 10

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,83 @@
 import express from "express";
+import postgres from "postgres";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { drizzle } from "drizzle-orm/postgres-js";
 
 import { handlerReadiness } from "./api/readiness.js";
-import { middlewareLogResponse, middlewareMetricsInc } from "./api/middleware.js";
-import { handlerMetrics, handlerResetMetrics } from "./api/metrics.js";
+import { handlerMetrics } from "./api/metrics.js";
+import { handlerReset } from "./api/reset.js";
+import {
+  errorMiddleWare,
+  middlewareLogResponse,
+  middlewareMetricsInc,
+} from "./api/middleware.js";
+import {
+  handlerChirpsCreate,
+  handlerChirpsDelete,
+  handlerChirpsGet,
+  handlerChirpsRetrieve,
+} from "./api/chirps.js";
+import { config } from "./config.js";
+import { handlerUsersCreate, handlerUsersUpdate } from "./api/users.js";
+import { handlerLogin, handlerRefresh, handlerRevoke } from "./api/auth.js";
+import { handlerWebhook } from "./api/webhooks.js";
+
+const migrationClient = postgres(config.db.url, { max: 1 });
+await migrate(drizzle(migrationClient), config.db.migrationConfig);
 
 const app = express();
-const PORT = 8080;
 
 app.use(middlewareLogResponse);
-app.use("/app", middlewareMetricsInc);
-app.use("/app", express.static("./src/app"));
+app.use(express.json());
 
-app.get("/api/healthz", handlerReadiness);
-app.get("/admin/metrics", handlerMetrics);
-app.get("/admin/reset", handlerResetMetrics);
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.get("/api/healthz", (req, res, next) => {
+  Promise.resolve(handlerReadiness(req, res)).catch(next);
+});
+app.get("/admin/metrics", (req, res, next) => {
+  Promise.resolve(handlerMetrics(req, res)).catch(next);
+});
+app.post("/admin/reset", (req, res, next) => {
+  Promise.resolve(handlerReset(req, res)).catch(next);
+});
+
+app.post("/api/polka/webhooks", (req, res, next) => {
+  Promise.resolve(handlerWebhook(req, res)).catch(next);
+});
+
+app.post("/api/login", (req, res, next) => {
+  Promise.resolve(handlerLogin(req, res)).catch(next);
+});
+app.post("/api/refresh", (req, res, next) => {
+  Promise.resolve(handlerRefresh(req, res)).catch(next);
+});
+app.post("/api/revoke", (req, res, next) => {
+  Promise.resolve(handlerRevoke(req, res)).catch(next);
+});
+
+app.post("/api/users", (req, res, next) => {
+  Promise.resolve(handlerUsersCreate(req, res)).catch(next);
+});
+app.put("/api/users", (req, res, next) => {
+  Promise.resolve(handlerUsersUpdate(req, res)).catch(next);
+});
+
+app.post("/api/chirps", (req, res, next) => {
+  Promise.resolve(handlerChirpsCreate(req, res)).catch(next);
+});
+app.get("/api/chirps", (req, res, next) => {
+  Promise.resolve(handlerChirpsRetrieve(req, res)).catch(next);
+});
+app.get("/api/chirps/:chirpId", (req, res, next) => {
+  Promise.resolve(handlerChirpsGet(req, res)).catch(next);
+});
+app.delete("/api/chirps/:chirpId", (req, res, next) => {
+  Promise.resolve(handlerChirpsDelete(req, res)).catch(next);
+});
+
+app.use(errorMiddleWare);
+
+app.listen(config.api.port, () => {
+  console.log(`Server is running at http://localhost:${config.api.port}`);
 });
